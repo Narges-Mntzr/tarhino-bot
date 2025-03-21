8d761f8e382e64bf48766ab08e8171ba547e26f8
nargesMntzr <narges.montazeri81@gmail.com>
2025-03-21 20:13:46 +0330

    Log error texts.

--- services/ai.py
+++ services/ai.py
@@ -1,4 +1,4 @@
-from config import AVAL_API_TOKEN
+from config import AVAL_API_TOKEN,logging
 from langchain_openai import ChatOpenAI
 
 model_name = "gpt-4o-mini"
@@ -20,5 +20,6 @@ def get_title_with_ai(message_text: str):
 
         responce = llm.invoke(messages)
         return responce.content
-    except Exception:
+    except Exception as e:
+        logging.error(e)
         return None
